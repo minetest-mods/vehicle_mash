92af52f52785b898b342e74347ae0af6aa2a8735
blert2112 <cdilts@bellsouth.net>
2016-08-11 00:45:53 -0400

    126r adjustments
    Enlarged model by 2.5x,
    adjusted collision box,
    increased braking and turning.

--- 126r.lua
+++ 126r.lua
@@ -3,11 +3,12 @@ local name = "car_126r"
 
 local definition = {
 	description = "126r car",
-	collisionbox = {-0.6, -0.25, -0.3, 0.6, 1/2, 0.3},
-	onplace_position_adj = -0.25,
+	collisionbox = {-0.7, -0.5, -0.7, 0.7, 0.7, 0.7},
+	onplace_position_adj = 0,
 	is_boat = false,
 	player_rotation = {x=0,y=90,z=0},
 	driver_attach_at = {x=-2,y=10,z=0},
+	driver_eye_offset = {x=0, y=0, z=0},
 	number_of_passengers = 0,
 	passenger_attach_at = {x=0,y=0,z=0},
 	passenger_eye_offset = {x=0, y=0, z=0},
@@ -22,8 +23,8 @@ local definition = {
 	max_speed_forward = 20,
 	max_speed_reverse = 10,
 	accel = 2,
-	braking = 4,
-	turn_speed = 2,
+	braking = 6,
+	turn_speed = 3,
 	drop_on_destroy = "",
 	recipe = {}
 }
